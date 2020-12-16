0b731d7cd193af1096c8b5cfb309111b0dd09b46
Max Grallinger <kellermeister.maxgrallinger@gmail.com>
2020-12-17 00:34:06 +0100

    fixed the grenades

--- ext/Client/__init__.lua
+++ ext/Client/__init__.lua
@@ -2,23 +2,6 @@
 local debug = false
 local activated = true
 local curStep = nil
-Hooks:Install(
-	"UI:PushScreen",
-	1,
-	function(hook, screen, priority, parentGraph, stateNodeGuid)
-		local screen = UIGraphAsset(screen)
-		print(screen.name)
-
-		if screen.name == "UI/Flow/Screen/KillScreen" and (pointOfAim.mode == 1 or pointOfAim.mode == 2) then
-			pointOfAim.mode = FiringMode.Disabled
-			Events:Unsubscribe("Player:UpdateInput")
-			Events:Unsubscribe("UpdateManager:Update")
-			Events:Unsubscribe("UI:DrawHud")
-			updateEvent = nil
-			drawHudEvent = nil
-		end
-	end
-)
 
 NetEvents:Subscribe("vu-ks-grenades:Success",function()
 	print("Grenades restocked successfully")
